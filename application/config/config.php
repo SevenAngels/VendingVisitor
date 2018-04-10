98df3352fc5b64f856f2e313ac44dba9f0a43db6
aritch17 <aritch17@uncc.edu>
2018-04-09 21:40:52 -0400

    Adjusted for heroku deployment

--- application/config/config.php
+++ application/config/config.php
@@ -30,7 +30,7 @@ $config['base_url'] = 'localhost';
 | Index File
 |--------------------------------------------------------------------------
 |
-| Typically this will be your map.php file, unless you've renamed it to
+| Typically this will be your index.php file, unless you've renamed it to
 | something else. If you are using mod_rewrite to remove the page set this
 | variable so that it is blank.
 |
