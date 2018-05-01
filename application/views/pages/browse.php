d805eba7582e2a1adc2a16a6244c1a7c67cca904
grigs1023 <36213776+grigs1023@users.noreply.github.com>
2018-05-01 17:00:07 -0400

    Add files via upload
    Styling of browse page

--- application/views/pages/browse.php
+++ application/views/pages/browse.php
@@ -6,13 +6,33 @@
  * Time: 8:20 PM
  */
 
-/** @var array $products SQL query result for all products to be displayed */
-foreach ($products as $product): ?>
-	<img src="/assets/imgs/products/<?php echo $product->ImageName ?>" alt="<?php echo $product->ImageName ?>">
-	<?php echo $product->Name ?>
-	<br>
-
-<?php endforeach; ?>
-
+/** @var array $products SQL query result for all products to be displayed Hello*/
+?>
+<main role="main">
+    <div class="album py-5 bg-light">
+        <div class="container">
+            <div class="row">
+                <?php foreach ($products as $product): ?>
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                        <img src="/assets/imgs/products/<?php echo $product->ImageName ?>" alt="<?php echo $product->ImageName ?>" class="rounded mx-auto d-block" height="300" width="120" >
+                             <div class="card-body">
+                                <p class="card-text"><?php echo $product->Name ?></p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">Directions</button>
+                                        </div>
+                                        <small class="text-muted">9 mins</small>
+                                    </div>
+                             </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+</main>
 </body>
 </html>
+
+
