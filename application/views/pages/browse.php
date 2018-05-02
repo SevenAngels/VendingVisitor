7ba2a6f08575708e48ad80a3602dc1e5886682dc
griggi <griggi1023@gmail.com>
2018-05-01 20:32:51 -0400

    test

--- application/views/pages/browse.php
+++ application/views/pages/browse.php
@@ -11,7 +11,7 @@
 <main role="main">
     <section class="jumbotron text-center">
         <div class="container">
-            <h1 class="jumbotron-heading">Browse Items</h1>
+            <h1 class="jumbotron-heading">Browser Items</h1>
         </div>
     </section>
     <div class="album py-5 bg-light">
