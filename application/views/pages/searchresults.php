ce61347e717ae954f4a13be4ca385e0fcf2e3a97
griggi <griggi1023@gmail.com>
2018-05-03 13:36:21 -0400

    styling of search page

--- application/views/pages/searchresults.php
+++ application/views/pages/searchresults.php
@@ -3,17 +3,39 @@
 /** @var array(Product) $results The results of the search query */
 ?>
 
-<h1>Search Results for <?php echo $query ?></h1>
-<ul>
-	<?php
-	foreach ($results as $product): ?>
-		<li>
-			<img src="/assets/imgs/products/<?php echo $product->ImageName ?>">
-			<?php echo "$product->Name $";
-			echo $product->Price / 100 ?>
-			<a href="/index.php/products/viewProduct/<?php echo $product->id ?>">
-				<button type="button" class="btn btn-sm btn-outline-secondary">View Product Info</button>
-			</a>
-		</li>
-	<?php endforeach; ?>
-</ul>
+<main role="main">
+<section class="jumbotron text-center">
+    <div class="container">
+        <h1 class="jumbotron-heading">Search Results for <?php echo $query ?></h1>
+    </div>
+</section>
+<div class="album py-5 bg-light">
+    <div class="container">
+        <div class="row">
+	        <?php foreach ($results as $product): ?>
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img src="/assets/imgs/products/<?php echo $product->ImageName ?>"
+                             alt="<?php echo $product->ImageName ?>" class="rounded mx-auto d-block" height="300">
+                        <div class="card-body">
+                            <p class="card-text"><?php echo $product->Name ?><br>$<?php echo $product->Price / 100 ?></p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <a href="/index.php/products/viewProduct/<?php echo $product->id ?>">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View
+                                            Product Info
+                                        </button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
+</main>
+</body>
+</html>
+
